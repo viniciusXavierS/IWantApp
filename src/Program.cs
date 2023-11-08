7f306aaf20618b27168f10c8ee25f30a409c5696
Vinicius Xavier <vinicius.xavier.silva@gmail.com>
2023-11-07 23:27:04 -0300

    Commit 3

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,12 @@
 using IWantApp.Endpoints.Categories;
 using IWantApp.Endpoints.Employees;
+using IWantApp.Endpoints.Security;
 using IWantApp.infra.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSqlServer<ApplicationDbContext>(
@@ -15,10 +20,43 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
     options.Password.RequiredLength = 3;
 }).AddEntityFrameworkStores<ApplicationDbContext>();
 
+builder.Services.AddAuthorization(options =>
+{
+    // Adicionar o authorize em todas as rotas para que sejam acessadas somente pelo token
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+      .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+      .RequireAuthenticatedUser()
+      .Build();
+}); 
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtBearerTokenSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtBearerTokenSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["JwtBearerTokenSettings:SecretKey"]))
+    };
+});
+
+
+builder.Services.AddScoped<QueryAllUsersWithClaimName>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
@@ -33,5 +71,6 @@ app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.H
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
 app.MapMethods(EmployeesPost.Template, EmployeesPost.Methods, EmployeesPost.Handle);
 app.MapMethods(EmployeesGetAll.Template, EmployeesGetAll.Methods, EmployeesGetAll.Handle);
+app.MapMethods(TokenPost.Template, TokenPost.Methods, TokenPost.Handle);
 
 app.Run();
---
+++ src/infra/Data/QueryAllUsersWithClaimName.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using IWantApp.Endpoints.Employees;
+using Microsoft.Data.SqlClient;
+
+namespace IWantApp.infra.Data;
+
+public class QueryAllUsersWithClaimName
+{
+    private readonly IConfiguration configuration;
+
+    public QueryAllUsersWithClaimName(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public IEnumerable<EmployeeResponse> Execute(int page, int rows)
+    {
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+        var query = @"SELECT Email, ClaimValue as Name
+                from AspNetUsers u inner 
+                Join AspNetUserClaims c 
+                on u.Id = c.UserId and ClaimType = 'Name'
+                order by name
+                OFFSET (@page -1 ) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+       return db.Query<EmployeeResponse>(
+          query,
+          new { page, rows }
+          );
+    }
+}
